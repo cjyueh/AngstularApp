375082c8878bc89634a565615ad2771afff2037b
cjyueh <cjyueh@gmail.com>
2016-03-27 22:30:55 -0700

    adds upvoting for comments

--- public/javascripts/PostsCtrl.js
+++ public/javascripts/PostsCtrl.js
@@ -2,4 +2,19 @@ app.controller('PostsCtrl', PostsCtrl);
 
 function PostsCtrl($scope, $stateParams, posts) {
   $scope.post = posts.posts[$stateParams.id]; //use $stateParams to get id of the post
+
+  $scope.addComment = function() {
+    if ($scope.body === "") {
+      return;
+    }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0,
+    });
+    $scope.body = ''; //clear form
+  };
+  $scope.incrementUpvotes = function(comment) {
+    comment.upvotes += 1;
+  };
 }
