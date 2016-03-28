9b5fae86d6f402d8cd331a388d11831ad6ff3ec8
cjyueh <cjyueh@gmail.com>
2016-03-27 18:28:10 -0700

    creates form for user input

--- app.js
+++ app.js
@@ -4,10 +4,23 @@ app.controller('MainCtrl', ['$scope', function($scope) {
   $scope.test = "Hello World!"; //check controller is linked to view
 
   $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
+    {question: 'post 1', upvotes: 5},
+    {question: 'post 2', upvotes: 2},
+    {question: 'post 3', upvotes: 15},
+    {question: 'post 4', upvotes: 9},
+    {question: 'post 5', upvotes: 4}
   ];
+
+  $scope.addPost = function() {
+    if (!$scope.question || $scope.question === '') {
+      return;
+    }
+    $scope.posts.push({question: $scope.question, upvotes: 4});
+    $scope.question = ''; //clear form
+  };
+
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
+  };
+
 }]);
